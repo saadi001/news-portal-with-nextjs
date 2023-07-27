35ac6d75345a6a3e1808f91558a5348ed5cc16d7
saadi001 <sheikhsadi1620@gmail.com>
2023-07-27 23:52:33 +0600

    fist commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
 
 
-const HomePage = () => {
+const HomePage = ({allNews}) => {
+  console.log(allNews);
   return (
     <>
       <Head>
@@ -24,3 +25,15 @@ export default HomePage;
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news")
+  const data = await res.json();
+
+  return {
+    props: {
+      allNews: data
+    }
+  }
+}
