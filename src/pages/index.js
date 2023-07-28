f5433b662c4d9e58d29d215390951575c12a9697
saadi001 <sheikhsadi1620@gmail.com>
2023-07-28 21:32:23 +0600

    redux implemantation explored

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
+import { useGetNewsesQuery } from "@/redux/api/api";
+import dynamic from "next/dynamic";
 
 const HomePage = ({ allNews }) => {
+  const {data, isLoading, isError, error} = useGetNewsesQuery()
+  // console.log(data);
+
+  const DynamicBanner = dynamic(() => import('@/components/UI/Banner'), {
+    loading: () => <h1>Loading...</h1>,
+  })
+
   return (
     <>
       <Head>
@@ -15,8 +23,8 @@ const HomePage = ({ allNews }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
-      <AllNews allNews={allNews}></AllNews>
+      <DynamicBanner />
+      <AllNews allNews={data}></AllNews>
     </>
   );
 };
